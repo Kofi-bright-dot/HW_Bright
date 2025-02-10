2a2ce64c408f5fed22f10de2d424266ef8b958ca
Bright Ladzro <kofiladzro@gmail.com>
2025-02-10 14:56:06 -0500

    Updated bright_hw.R with mean computation

--- bright_hw.R
+++ bright_hw.R
@@ -1,6 +1,22 @@
-# Generate 100 random numbers from a normal distribution
+library(tidyverse)
+# Generating 100 random numbers from a normal distribution
 random_numbers <- rnorm(100, mean = 25, sd = 1)
 
-# Print the first 10 numbers
-print(head(random_numbers, 10)
+# Printing the first 10 numbers
+print(head(random_numbers, 10))
 
+#[ 25.13765 25.28544 25.44578 24.84498 25.82346 25.27011 24.35326 24.89380 24.17942 24.03147]
+
+
+# 100 random numbers from a normal distribution
+random_numbers <- rnorm(100, mean = 25, sd = 1)
+
+print(head(random_numbers, 10))
+# [25.13765 25.28544 25.44578 24.84498 25.82346 25.27011 24.35326 24.89380 24.17942 24.03147]
+
+# Calculating the mean of the 100 random numbers
+mean_value <- mean(random_numbers)
+
+print(mean_value)
+
+# [25.05259]
